adcf534de45ee43e0555dba3285a7d47896a888d
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-02-14 09:46:38 +0530

    Added Styling, border and Font related details

---
+++ 29 Add Font/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:"class",
+  content: ["*"],
+  theme: {
+
+    extend: {
+     fontFamily:{
+      guerrilla:["Protest Guerrilla", 'sans-serif'],
+      playwrite: ["Playwrite CU", 'cursive']
+     }
+    },
+    container: {
+      center: true
+    }
+  },
+  plugins: [],
+}
+
+
