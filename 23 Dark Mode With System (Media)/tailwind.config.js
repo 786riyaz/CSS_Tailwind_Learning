e8f397a258e407d0a5f1cd3ca9a4dc468d39b6b0
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-02-14 09:45:26 +0530

    Added Dark mode related Details

---
+++ 23 Dark Mode With System (Media)/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:"media",
+  content: ["*"],
+  theme: {
+
+    extend: {
+      spacing: {
+        '21': '6rem',
+        '22': '3rem',
+        '13':'13px',
+        'px-8':'8px',
+        'px-100':'100px'
+      }
+    },
+    container: {
+      center: true
+    }
+  },
+  plugins: [],
+}
+
+
